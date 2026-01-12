1bdfce8116eb2090855f4d10f9339874cd61bee7
Karen <karen@example.com>
2026-01-12 15:19:37 -0500

    agregando math-promedio-moda-y-mediana

---
+++ descuentos.js
@@ -0,0 +1,85 @@
+
+// let inputprecio = document.getElementById("precio");
+// let inputdescuento = document.getElementById("dscto")
+// let p = document.getElementById("p")
+// let botonresultador = document.getElementById("calcularDescuento")
+
+let inputprecio = document.getElementById("precio")
+let inputdescuento = document.getElementById("dscto")
+let p = document.getElementById("descuento_total")
+let botonresultador = document.getElementById("calcular");
+botonresultador.addEventListener('click', calculaPrecio)
+
+
+// const objDescuento = {
+//     platzivacation: 10,
+//     mothersday: 15,
+//     independenceday: 20,
+//     christmas: 50,
+// } ///dfg 
+const objDescuento = [];
+objDescuento.push({
+  name: 'JuanDC_es_Batman',
+  dscto: 30,
+});
+objDescuento.push({
+  name: 'pero_es_un_secreto',
+  dscto: 25,
+});
+objDescuento.push({
+  name: 'no_le_digas_a_nadie',
+  dscto: 15,
+});
+
+function calculaPrecio(){
+    const precio = Number(inputprecio.value);
+    const descuento = (inputdescuento.value);
+    const price = (precio)
+    let discount;
+    // const objDescuento = (descuento)
+    const buscarArray = objDescuento.find((element)=>element.name == descuento )
+
+    if(buscarArray){
+        discount = buscarArray.dscto
+        const Resultado = (price * (100 - discount)) / 100;
+        p.innerText = 'El nuevo precio con descuento es S/' + Resultado;
+    }else{
+        p.innerText = 'No existe el cupón de descuento o ya expiró';
+    }
+   
+    // if(objDescuento[descuento]){
+    //     discount = objDescuento[descuento]
+    //     const Resultado = (price * (100 - discount)) / 100;
+    //     p.innerText = 'El nuevo precio con descuento es S/' + Resultado;
+    // }else{
+    //     p.innerText = 'No existe el cupón de descuento o ya expiró';
+    // }
+}
+
+
+
+// const inputPrice = document.querySelector('#price');
+// const inputDiscount = document.querySelector('#discount');
+// const btn = document.querySelector('#calcular');
+// const pResult = document.querySelector('#descuento_total');
+
+// btn.addEventListener('click', calcularPrecioConDescuento);
+
+// function calcularPrecioConDescuento() {
+//   const price = Number(inputPrice.value);
+//   const discount = Number(inputDiscount.value);
+
+//   if (price <= 0 || discount < 0) {
+//     pResult.innerText = 'CHANCLA por favor llena el formulario';
+//     return;
+//   }
+
+//   if (discount > 100) {
+//     pResult.innerText = 'Ajá, ya quisieras, no te vamos a dar plata, ¡PAGA!';
+//     return;
+//   }
+
+//   const precioFinal = price - (price * discount / 100);
+//   pResult.innerText = `Precio final: $${precioFinal}`;
+// }
+
---
+++ math.js
@@ -0,0 +1,17 @@
+let arrya =[]
+function promedio(arrya) {
+    return arrya.reduce((acum,valor)=>acum+valor,0)/arrya.length
+} 
+console.log(promedio([1,2,3,4,5,6]))
+
+function mediana(arrya){
+    let cantidad=arrya.sort((a,b)=>a-b).length
+    if(arrya.length % 2 == 0){
+        return (arrya[(cantidad)/2-1] + arrya[(cantidad)/2])/2
+    }else{
+        return arrya[Math.ceil((cantidad)/2-1)]
+    }
+}
+
+console.log(mediana([1,1,2,3,14,1,2,4,8]))
+console.log(mediana([3,2,1,0]))
